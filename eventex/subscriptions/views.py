faacdd691384b192891d85aee9ff4eb5a062f800
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-17 20:05:03 -0300

    Thank you page

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -1,7 +1,6 @@
 from django.conf import settings
-from django.contrib import messages
 from django.core import mail
-from django.http import HttpResponseRedirect
+from django.http import Http404, HttpResponseRedirect
 from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
@@ -23,27 +22,29 @@ def create(request):
     if not form.is_valid():
         return render(request,  'subscriptions/subscription_form.html', {'form': form})
 
-    form.full_clean()
-
-    subscriber_email = form.cleaned_data['email']
+    subscription = Subscription.objects.create(**form.cleaned_data)
 
     # Send email
     _send_email('Confirmação de inscrição',
                 settings.DEFAULT_FROM_EMAIL,
-                subscriber_email,
+                subscription.email,
                 'subscriptions/subscription_email.txt',
-                form.cleaned_data)
+                {'subscription': subscription})
 
-    Subscription.objects.create(**form.cleaned_data)
-
-    # Success feedback
-    messages.success(request, 'Inscrição realizada com sucesso!')
-
-    return HttpResponseRedirect('/inscricao/')
+    return HttpResponseRedirect(f'/inscricao/{subscription.pk}/')
 
 
 def new(request):
     return render(request, 'subscriptions/subscription_form.html', {'form': SubscriptionForm()})
+
+
+def detail(request, pk):
+    try:
+        subscription = Subscription.objects.get(pk=pk)
+    except Subscription.DoesNotExist:
+        raise Http404()
+
+    return render(request, 'subscriptions/subscription_detail.html', {'subscription': subscription})
 
 
 def _send_email(subject, from_, to, template_name, context):
