27a6c3766ec21583ec4fc188b1a3bf5830c832b8
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-24 18:59:21 -0300

    Padroniza nomes

--- eventex/subscriptions/tests/test_form_subscription.py
+++ eventex/subscriptions/tests/test_form_subscription.py
@@ -24,6 +24,15 @@ class SubsciptionFormTest(TestCase):
 
         self.assertFormErrorCode(form, 'cpf', 'length')
 
+    def test_name_must_be_capitalized(self):
+        '''Name must be capitalized.'''
+        # Henrique bastos -> Henrique Bastos
+
+        form = self.make_validated_form(name='HENRIQUE bastos')
+
+        self.assertEqual('Henrique Bastos', form.cleaned_data['name'])
+
+
     def assertFormErrorCode(self, form, field, code):
         errors = form.errors.as_data()
         errors_list = errors[field]
