c267f739c1d7b4499eaed4a7b515d4b1ffabcf05
Henrique Andrade <henrique.ccda@gmail.com>
2022-04-28 15:27:29 -0300

    Simplifica com modelform.

---
+++ eventex/subscriptions/validators.py
@@ -0,0 +1,12 @@
+
+
+
+from django.forms import ValidationError
+
+
+def validate_cpf(value):
+    if not value.isdigit():
+        raise ValidationError('CPF deve conter apenas números.', 'digits')
+
+    if len(value) != 11:
+         raise ValidationError('CPF deve ter 11 números.', 'length')
